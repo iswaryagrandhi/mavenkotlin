03931a97b16fafaa9140cad5a7d5721cae93331f
iswarya <iswarya@TeamCity>
2021-01-05 10:14:10 +0530

    TeamCity change in 'Mavenkotlin' project: runners of 'test' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -40,7 +40,7 @@ object Build : BuildType({
 
     steps {
         maven {
-            goals = "clean compile"
+            goals = "clean test"
             pomLocation = ".teamcity/pom.xml"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
